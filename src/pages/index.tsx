ef3ea36bea95e6e5831c37c5b1b247c7e474a0fb
Marlon Menegatti <marlonmenegatt@gmail.com>
2021-04-11 20:01:57 -0300

    Initial Commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Codeby</title>
+      </Head>
+
+      <h1>Desafio</h1>
+    </div>
+  )
+}
